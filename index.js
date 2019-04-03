82d5f239a744ab95062b7a4edc55189565f65e77
HyunJin Lee <hjleesm@naver.com>
2019-04-03 18:08:22 +0900

    Fixed error
    - Fixed install problem
    - Update dependencies

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ var fs = require('fs')
   , vm = require('vm')
   , jsdom = require('jsdom')
 
+const { JSDOM } = jsdom;
+
 var script = loadRaphael()
 module.exports.generate = function generate(width, height, callback) {
-    var win = jsdom.createWindow(jsdom.dom)
-        , doc = jsdom.jsdom("<html><body></body></html>")
+    var doc = new JSDOM('<html><body></body></html>')
+        win = doc.window
     var nav = win.navigator
     win.document = doc
-    doc.implementation.addFeature(
-        "http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1")
-    paper = extractRaphael(win, doc, nav)(0, 0, width || 42, height || 42)
-    if (callback) callback(paper)
-    return doc.body.firstChild && doc.body.firstChild.outerHTML || ""
+
+    // doc.implementation.addFeature(
+    //     "http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1")
+    var paper = extractRaphael(win, win.document, nav)(0, 0, width || 42, height || 42)
+
+    if (callback) callback(paper, win.document, nav)
+    return win.document.body.firstChild && win.document.body.firstChild.outerHTML || ""
 }
 
 function loadRaphael() {
